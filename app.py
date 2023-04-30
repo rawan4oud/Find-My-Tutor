1af8a784589d57cca954432a59aa26e2a651e730
Charbel Azar <77696672+Charbel-Azar@users.noreply.github.com>
2023-05-01 00:42:47 +0300

    chat

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, session, send_file
+from flask_socketio import SocketIO, join_room, leave_room, emit
+from flask_session import Session
 import mysql.connector
 from werkzeug.utils import secure_filename
 from controller import *
 import os
+
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'secret'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+Session(app)
+
+socketio = SocketIO(app, manage_session=False)
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
@@ -100,10 +112,6 @@ def home():
     return render_template("home.html")
 
 
-@app.route('/logout')
-def logout():
-    session.clear()
-    return render_template("home.html")
 
 
 @app.route('/studentprofile.html')
@@ -118,6 +126,17 @@ def profile():
         return "Error: student not found"
 
 
+@app.route('/tutorprofile.html')
+def tutorprofile():
+    username = session['username']
+
+    tutor = get_tutor_instance(username)
+
+    if tutor:
+        return render_template('tutorprofile.html', tutor=tutor)
+    else:
+        return "Error: student not found"
+
 @app.route('/tutorsearch.html')
 def tutorsearch():
     username = request.args.get('username')
@@ -126,6 +145,10 @@ def tutorsearch():
     return render_template('tutorsearch.html', tutor=tutor, username=username)
 
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template("home.html")
 
 @app.route('/login.html', methods=['GET', 'POST'])
 def login():
@@ -516,6 +539,40 @@ def download_doc(doc_name):
 
     # Return the file to the user for download
     return send_file(file_path, as_attachment=True)
+
+
+
+@app.route('/chat', methods=['GET', 'POST'])
+def chat():
+
+        username = request.form['karim@lau.edu']
+        room = request.form['dany@lau.edu']
+        #Store the data in session
+        session['username'] = username
+        session['room'] = room
+        return render_template('chat.html', session = session)
+
+
+@socketio.on('join', namespace='/chat')
+def join(message):
+    room = session.get('room')
+    join_room(room)
+    emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=room)
+
+
+@socketio.on('text', namespace='/chat')
+def text(message):
+    room = session.get('room')
+    emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=room)
+
+
+@socketio.on('left', namespace='/chat')
+def left(message):
+    room = session.get('room')
+    username = session.get('username')
+    leave_room(room)
+    session.clear()
+    emit('status', {'msg': username + ' has left the room.'}, room=room)
 
 
 if __name__ == '__main__':
